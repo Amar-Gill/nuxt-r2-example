a964106db69d82bfcf3994a5d97140729ef1bd16
Amar Gill <amar.s.gill@outlook.com>
2024-05-14 22:40:05 -0400

    add prefix to upload key

--- server/api/image.put.ts
+++ server/api/image.put.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
   const r2Object = await event.context.cloudflare.env.MY_BUCKET.put(
-    "ballz",
+    "mega/ballz",
     "TODO",
   );
   console.info("Uploaded object to R2", { r2Object });
